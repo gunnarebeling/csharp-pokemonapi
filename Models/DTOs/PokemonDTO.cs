ed0521498225f836315fc07930abe7da3c7e60a2
gunnar ebeling <gunnarebeling@Mac-mini.local>
2024-11-05 09:54:14 -0600

    added classes

---
+++ Models/DTOs/PokemonDTO.cs
@@ -0,0 +1,10 @@
+namespace PokemonAPI.Modules.DTOs;
+public class PokemonDTO
+    {
+        public string Name { get; set; }
+        
+        public decimal Price { get; set; }
+
+        public int Rarity { get; set; }
+
+    }
---
+++ Models/Pokemon.cs
@@ -0,0 +1,10 @@
+namespace PokemonAPI.Modules;
+public class Pokemon
+    {
+        public string Name { get; set; }
+        
+        public decimal Price { get; set; }
+
+        public int Rarity { get; set; }
+
+    }
